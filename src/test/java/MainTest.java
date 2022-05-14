1609bbc6006c05808a4087e44c1793bdd76231ae
Paweł Frankiewicz <pawel.frankiewicz@eyzee.com>
2022-05-14 11:55:21 +0200

    Add null test.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -18,4 +18,35 @@ public class MainTest {
     public void reminder_test() {
         assertEquals(1D, Main.performMathOperation(OperationType.REMINDER, 11D, 2D));
     }
+
+    @Test
+    public void null_test() {
+        try {
+            Main.performMathOperation(null, 11D, 2D);
+            fail();
+        } catch (RuntimeException e) {
+            assertTrue(true);
+        }
+
+        try {
+            Main.performMathOperation(OperationType.MULTIPLY, null, null);
+            fail();
+        } catch (RuntimeException e) {
+            assertTrue(true);
+        }
+
+        try {
+            Main.performMathOperation(null, null, null);
+            fail();
+        } catch (RuntimeException e) {
+            assertTrue(true);
+        }
+
+        try {
+            Main.performMathOperation(OperationType.DIVIDE, null, 123D);
+            fail();
+        } catch (RuntimeException e) {
+            assertTrue(true);
+        }
+    }
 }
